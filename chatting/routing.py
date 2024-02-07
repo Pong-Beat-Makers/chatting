c677d5699f58d614e2e393524591d091e9a50941
sunwooiljoon <wnsdl1659@naver.com>
2024-02-07 17:32:07 +0900

    fix : 대상 닉네임 Path에서 제거
    path를 통해서 하지 않고 메인 그룹에서 json에 포함시키는 방식으로 변환

--- chatting/routing.py
+++ chatting/routing.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-    path("ws/chatting/<target_nickname>/", consumers.ChattingConsumer.as_asgi()),
+    path("ws/chatting/", consumers.ChattingConsumer.as_asgi()),
 ]
